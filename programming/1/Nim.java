d07e616f5cabe850ec061e7170549b39409d537a
Kmaschm <96199973+Kmaschm@users.noreply.github.com>
2022-07-09 14:55:42 +0000

    Updated comments for Nim with Current Status and Possible Changes to Make

--- programming/1/Nim.java
+++ programming/1/Nim.java
@@ -15,6 +15,26 @@ import java.io.*;
 import java.util.*;
 
 
+/* CURRENT STATUS:
+* Nim runs without errors
+* Nim's algorithm:
+* * game ends when number of stones <= 0.
+* * Human player always goes 1st 
+* ** Program will check that human is selecting only 1-3 stones (continuously warn and prompt if not) 
+* ** Program does NOT YET double check that there are sufficient stones)
+* * AI will choose (4 - # of stones human chose) each time
+* ** this will result in AI winning each time
+* * after each turn, program checks if there are 0 stones left.  If yes, program outputs  the player who just went has won.
+*
+****************
+*
+POSSIBLE CHANGES TO MAKE:
+* let human player choose who will go first (or some randomization of who goes first)
+* AI chooses randomly (with safeguards for sufficient stones, etc.)
+*
+*/
+
+
 public class Nim {
 
   public static void main(String[] args) {  
