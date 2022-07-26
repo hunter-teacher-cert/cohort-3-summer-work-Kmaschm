6e261f3d6936298c3c489323a20097dd9da1d9f7
Kmaschm <96199973+Kmaschm@users.noreply.github.com>
2022-07-26 15:04:47 +0000

    Up to mergeSort completed & tested

--- ds/sorting/SortSearch.java
+++ ds/sorting/SortSearch.java
@@ -201,18 +201,16 @@ public class SortSearch{
     
       int midIndex = (lowIndex + highIndex) / 2;
   
-      if (lowIndex >=  highIndex) { // end here
+      if (lowIndex >  highIndex) { // end here
         System.out.println("Value, " + value + ", not found.");
         return NOT_FOUND;
       } else if( this.get(midIndex) == value) { // target found
         return midIndex;
-      } else if (value < this.get(midIndex)) { 
-        // value at mid index higher than target
+      }  else if (value < this.get(midIndex)) { 
         // we need to look at the lower half, so set highIndex to 1 below current middle
-        highIndex = midIndex;
+        highIndex = midIndex - 1;
       } else { 
       //if (value > this.get(midIndex) ) { 
-          // value at mid index lower than target
         // we need to look at the upper half, so set lowIndex to 1 above middle location
         lowIndex = midIndex + 1;
         
@@ -232,7 +230,122 @@ public class SortSearch{
 	    Collections.sort(data);
 	
     }
-    
 
+  /* Merge Sort Stuff after here */
+    /**
+       Builds and returns an ArrayList that's already in increasing order.
+       You can use this method to test your merge method.
+    */
+    public ArrayList<Integer> buildIncreasingList(int size){
+	ArrayList<Integer>  newlist = new ArrayList<Integer>();
+	Random r = new Random();
+	int nextval = r.nextInt(20)+1;
+	for (int i=0;i<size;i++){
+	    newlist.add(nextval);
+	    nextval = nextval + r.nextInt(20);
+	}
+
+	return newlist;
+	}
+
+    /**
+       this routine should create and return a new ArrayList of
+       integers and fill it by merging list1 and list2 into the new
+       list.
+       list1 and list2 are already sorted in increasing order.
+       Example:
+       If list1 contains [1,5,17,25]
+       and list2 contains [3,6,10,30,40,50]
+       The new list will contain:
+       [1, 3, 5, 6, 10, 17, 25, 30, 40, 50]
+       
+    */
+
+
+  // Worked with Mike Randazzo, Seth Adams, and Alise Braick on merge
+  public ArrayList<Integer> merge(ArrayList<Integer> list1,
+				    ArrayList<Integer> list2){
+      // code for merge
+      ArrayList<Integer> mergedData = new ArrayList<Integer>();
+      int counter1 = 0, counter2 = 0;
+      int size1 = list1.size();
+      int size2 = list2.size();
+      while(counter1 < size1 && counter2 < size2) {
+          if(list1.get(counter1) < list2.get(counter2)) {
+              mergedData.add(list1.get(counter1));
+              counter1++; 
+          } else {
+              mergedData.add(list2.get(counter2));
+              counter2++; 
+          }
+       }
+       if(size1 > counter1) {
+         for(int i = counter1; i < size1; i++) {
+            mergedData.add(list1.get(i));
+         }
+       }
+      if(size2 > counter2) {
+        for(int j = counter2; j < size2; j++) {
+          mergedData.add(list2.get(j));
+        }
+      } 
+       return mergedData;
+}
+    
+    //   ArrayList<Integer> mergedList = new ArrayList<Integer>();
+
+    //   while (list1.size() > 0 && list2.size() > 0 ) { // while both lists are not empty 
+    //     if(list1.get(0) < list2.get(0)) {
+    //      mergedList.add(list1.get(0));
+    //      list1.remove(0);
+    //    } else {
+    //      mergedList.add(list2.get(0));
+    //      list2.remove(0);
+    //    }    
+    //   }
+    //   if(list1.size() > 0) {
+    //     for(int i = 0; i < list1.size(); i++) {
+    //       mergedList.add(list1.get(i));
+    //     }
+    //   } 
+    //   if(list2.size() > 0) {
+    //      for(int i = 0; i < list2.size(); i++) {
+    //       mergedList.add(list2.get(i));
+    //     }
+    //   }
+	   //  return mergedList; 
+    // }
+
+
+  
+public ArrayList<Integer> mergeSort(ArrayList<Integer> list){
+      int size = list.size();
+      // check for base case
+      if(size < 2) {
+        return list;
+      } else {
+        ArrayList<Integer> listLeft = new ArrayList<Integer>(list.subList(0, size/2)); // lower half
+        ArrayList<Integer> listRight = new ArrayList<Integer>(list.subList(size/2, size)); // upper half
+        ArrayList<Integer> sortedLeft = mergeSort(listLeft); // sorted lower
+        ArrayList<Integer> sortedRight = mergeSort(listRight); // sorted upper
+        ArrayList<Integer> sorted = merge(sortedLeft, sortedRight);
+        return sorted;
+      }
+    
+      // if not the base case
+      // split in two lists
+      // meregSort the left half
+      // mergeSort the right half
+      // merge them together into a new list
+      // return that new list 
+
+  }
+
+
+  public void msort(){
+      data = mergeSort(data);
+  }    
+
+  
     
 }
--- ds/sorting/SortSearchDriver.java
+++ ds/sorting/SortSearchDriver.java
@@ -4,45 +4,61 @@ import java.util.*;
 public class SortSearchDriver {
     public static void main(String[] args) {
 
+ //      int size = 20;
+	//   SortSearch ss = new SortSearch(size);
+      
+ //      ArrayList<Integer> a = ss.buildIncreasingList(20);
+ //    	System.out.println(a);
 
-	// Uncomment these to test part 1
-	int size = 100000;
-	SortSearch ss = new SortSearch(size);
-	//System.out.println(ss);
+	// // build a second Arralist here
+ //      ArrayList<Integer> b = ss.buildIncreasingList(20);
+ //    	System.out.println(b);
 	
-  long start,elapsed;
 
-  System.out.println("Testing Linear Search");
-	start = System.currentTimeMillis();
-  System.out.println("start " + start);
-  // ss.linearSearch(12);
-  System.out.println(ss.linearSearch(12));
-	elapsed = System.currentTimeMillis() - start;   
-	System.out.println("Size: " + size + " Time: " + elapsed);
+	// // test your merge routine here
+ //      ArrayList<Integer> merged = ss.merge(a, b);
+ //      System.out.println(merged);
 
-  System.out.println("Testing Sort");
-  start = System.currentTimeMillis();
-  System.out.println("start " + start);
-	ss.sort();
-  //System.out.println(ss);
-	elapsed = System.currentTimeMillis() - start;
-	System.out.println("Size: " + size + " Time: " + elapsed);
 
-  System.out.println("Testing Binary Search");
-  start = System.currentTimeMillis();
-  System.out.println("start " + start);
-  // ss.binarySearch(12);
-  System.out.println(ss.binarySearch(12));
-	elapsed = System.currentTimeMillis() - start; //only the binary search time 
-	System.out.println("Size: " + size + " Time: " + elapsed);
+      
+	// Uncomment these to test part 1
+	// int size = 1000;
+	// SortSearch ss = new SortSearch(size);
+	// //System.out.println(ss);
+	
+ //  long start,elapsed;
 
-  System.out.println("Testing Recursive Binary Search");
-  start = System.currentTimeMillis();
-  System.out.println("start " + start);
-  // ss.binarySearchRecursive(12, 0, size-1);
-  System.out.println(ss.binarySearchRecursive(12, 0, size-1));
-	elapsed = System.currentTimeMillis() - start; //only the recursive binary search time 
-	System.out.println("Size: " + size + " Time: " + elapsed);
+ //  System.out.println("Testing Linear Search");
+	// start = System.currentTimeMillis();
+ //  System.out.println("start " + start);
+ //  // ss.linearSearch(12);
+ //  System.out.println(ss.linearSearch(12));
+	// elapsed = System.currentTimeMillis() - start;   
+	// System.out.println("Size: " + size + " Time: " + elapsed);
+
+ //  System.out.println("Testing Sort");
+ //  start = System.currentTimeMillis();
+ //  System.out.println("start " + start);
+	// ss.sort();
+ //  //System.out.println(ss);
+	// elapsed = System.currentTimeMillis() - start;
+	// System.out.println("Size: " + size + " Time: " + elapsed);
+
+ //  System.out.println("Testing Binary Search");
+ //  start = System.currentTimeMillis();
+ //  System.out.println("start " + start);
+ //  // ss.binarySearch(12);
+ //  System.out.println(ss.binarySearch(12));
+	// elapsed = System.currentTimeMillis() - start; //only the binary search time 
+	// System.out.println("Size: " + size + " Time: " + elapsed);
+
+ //  System.out.println("Testing Recursive Binary Search");
+ //  start = System.currentTimeMillis();
+ //  System.out.println("start " + start);
+ //  // ss.binarySearchRecursive(12, 0, size-1);
+ //  System.out.println(ss.binarySearchRecursive(12, 0, size-1));
+	// elapsed = System.currentTimeMillis() - start; //only the recursive binary search time 
+	// System.out.println("Size: " + size + " Time: " + elapsed);
 
       
 		
@@ -68,7 +84,29 @@ public class SortSearchDriver {
  //  System.out.println("Binary search:" + ss.binarySearch(4));
  //  System.out.println("Recursive binary search:" + ss.binarySearchRecursive(4, 0, 14));
       
-	
+	 int size = 50000;
+    long start,elapsed;
+	  start = System.currentTimeMillis();
+	    // put what you want to test here!
+    SortSearch test = new SortSearch(size);
+    // System.out.println(test); 
+    // System.out.println("sorting"); 
+    test.sort();
+    // System.out.println(test); 
+	  elapsed = System.currentTimeMillis() - start;
+	  System.out.println("Size: " + size + " Time: " + elapsed);
+      
+
+    start = System.currentTimeMillis();
+	    // put what you want to test here!
+    test = new SortSearch(size);
+    //System.out.println(test); 
+   // System.out.println("sorting"); 
+    test.msort();
+    // System.out.println(test); 
+	  elapsed = System.currentTimeMillis() - start;
+	  System.out.println("Size: " + size + " Time: " + elapsed);
+          
 
 
 	      
