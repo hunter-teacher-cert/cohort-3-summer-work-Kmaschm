aabbbeac1bfb1a13a041ac7cda4210ac044cfff3
Kmaschm <96199973+Kmaschm@users.noreply.github.com>
2022-07-28 15:24:40 +0000

    Set up files for stacks & queues

---
+++ ds/stacksqueues/Queue.java
@@ -0,0 +1,53 @@
+public class Queue{
+
+  //Recall, a queue is a First In First Out structure.
+  
+  // Place private instance variables here
+
+  // Place constructors here
+  // A constructor with no parameters
+  public Queue() {
+    
+  }
+
+
+    // Place methods here
+
+  
+  // void enqueue(int value) - add value to the end of the queue
+  public void enqueue(int value) {
+    
+  }
+  
+    
+  // int dequeue() - remove and return the top value from front of the queue
+  public int dequeue() {
+    return -1; // change this 
+  }
+    
+  // int front() - return but do not remove the top value from the front of the queue
+  public int front() {
+    return -1; // change this 
+  }
+    
+  // boolean isEmpty() - return true of the queue is empty, false otherwise
+  public boolean isEmpty() {
+    return false; // change this
+  }
+    
+  // int size() - return the number of elements currently in the queue
+  public int size() {
+    return -1; // change this 
+  }
+    
+  // boolean isFull() - returns true if the queue is full, false otherwise - only implement this if you use an array for implementation
+  public boolean isFull() {
+    return false; // change this
+  }
+
+  public String toString() {
+    return ""; // change this
+  }
+  
+
+}
---
+++ ds/stacksqueues/Stack.java
@@ -0,0 +1,53 @@
+public class Stack{
+
+  //Recall, a stack is a Last In First Out structure.
+    // Place private instance variables here
+
+    // Place constructors here
+  
+    // A constructor with no parameters
+    public Stack() {
+  
+    }
+
+    // Place methods here
+  // You should implement the following methods
+  
+  // void push(int value) - add value to the top of the stack
+  public void push(int value) {
+    
+  }
+  
+  // int pop() - remove and return the top value from the stack
+  public int pop() {
+    return -1; // change this
+  }
+  
+  // int top() - return but do not remove the top value from the stack
+  public int top() {
+    return -1;  //change this
+  }
+  
+  // boolean isEmpty() - return true of the stack is empty, false otherwise
+  public boolean isEmpty() {
+    return false; // change this
+  }
+
+  // int size() - return the number of elements currently in the stack
+  public int size() {
+    return -1; // change this
+  }
+  
+  // boolean isFull() - returns true if the stack is full, false otherwise - only implement this if you use an array for implementation
+  public boolean isFull() {
+    return false; // change this
+  }
+  
+  // String toString() 
+  public String toString() {
+    return ""; // change this
+  }
+
+  
+
+}
